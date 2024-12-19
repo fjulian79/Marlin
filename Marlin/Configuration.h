6c82c8caa2d6cb452805967ee56564e54d334040
Julian Friedrich <julian.friedrich@gmx.at>
2024-12-19 11:32:37 +0100

    New default PID values given by M303 E0 C8 S210

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -684,9 +684,9 @@
     #define DEFAULT_Ki_LIST {   1.08,   1.08 }
     #define DEFAULT_Kd_LIST { 114.00, 114.00 }
   #else
-    #define DEFAULT_Kp  39.00
-    #define DEFAULT_Ki   4.69
-    #define DEFAULT_Kd  81.20
+    #define DEFAULT_Kp  27.36
+    #define DEFAULT_Ki   2.31
+    #define DEFAULT_Kd  81.00
   #endif
 #else
   #define BANG_MAX 255    // Limit hotend current while in bang-bang mode; 255=full current
